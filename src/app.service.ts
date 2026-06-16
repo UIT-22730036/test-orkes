3d81b3493781256070d97ca791b35f70a8328540
Huynh Ngoc An Khang <ankhang261096@gmail.com>
2026-06-16 23:44:51 +0700

    init

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  private readonly logger = new Logger(AppService.name);
+  constructor(private readonly mailService: MailerService) {}
+  async getHello(): Promise<string> {
+    await this.mailService.sendMail({
+      to: 'ankhang261096@gmail.com',
+      subject: 'Welcome to Our App!',
+      template: './mail/templates/welcome', // Points to src/mail/templates/welcome.hbs
+      context: {
+        name: 'An Khang', // Variables passed into the Handlebars template
+      },
+    });
+    this.logger.log(
+      `Welcome email successfully sent to ankhang261096@gmail.com`,
+    );
     return 'Hello World!';
   }
 }
